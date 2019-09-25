74afa24b65b6fab4120db4890d06b4d5ad0b802e
yutasb <shibamoto.yuta@gmail.com>
2019-09-26 00:29:35 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    newContent: "",
+    content2: [],
+    newMoney: "",
+    money2: []
+  },
+  methods: {
+    addContent: function() {
+      if (this.newContent === "") return;
+      var content = {
+        item: this.newContent,
+        isDOne: false
+      };
+      this.content2.push(content);
+      this.newContent = "";
+    },
+    addMoney: function() {
+      if (this.newMoney === "") return;
+      var money = {
+        item2: this.newMoney,
+        isDone: false
+      };
+      this.money2.push(money);
+      this.newMoney = "";
+    }
+  }
+});
