96f6191b79525fc78113b30431733f9d3867b8c2
yutasb <shibamoto.yuta@gmail.com>
2019-10-27 18:56:04 +0900

    localstrage実装

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ var app = new Vue({
     content2: [],
     newMoney: "",
     money2: [],
-    sum: 0,
+    sum: "",
     newContent2: "",
     content3: [],
     newMoney2: "",
@@ -34,6 +34,46 @@ var app = new Vue({
     if (localStorage.name2) {
       this.name2 = localStorage.name2;
     }
+    //1人目
+    if (localStorage.getItem("content2")) {
+      try {
+        this.content2 = JSON.parse(localStorage.getItem("content2"));
+      } catch (e) {
+        localStorage.removeItem("content2");
+      }
+    }
+    if (localStorage.getItem("money2")) {
+      try {
+        this.money2 = JSON.parse(localStorage.getItem("money2"));
+      } catch (e) {
+        localStorage.removeItem("money2");
+      }
+    }
+    if (localStorage.sum) {
+      this.sum = localStorage.sum;
+    }
+    //2人目
+    if (localStorage.getItem("content3")) {
+      try {
+        this.content3 = JSON.parse(localStorage.getItem("content3"));
+      } catch (e) {
+        localStorage.removeItem("content3");
+      }
+    }
+    if (localStorage.getItem("money3")) {
+      try {
+        this.money3 = JSON.parse(localStorage.getItem("money3"));
+      } catch (e) {
+        localStorage.removeItem("money3");
+      }
+    }
+    if (localStorage.sum2) {
+      this.sum2 = localStorage.sum2;
+    }
+    //合計
+    if (localStorage.total) {
+      this.total = localStorage.total;
+    }
   },
   watch: {
     name(newName) {
@@ -42,12 +82,18 @@ var app = new Vue({
     name2(newName2) {
       localStorage.name2 = newName2;
     },
-    money2: {
-      handler: function() {},
-      deep: true
+    sum(newSum) {
+      localStorage.sum = newSum;
+    },
+    sum2(newSum2) {
+      localStorage.sum2 = newSum2;
+    },
+    total(newTotal) {
+      localStorage.total = newTotal;
     }
   },
   methods: {
+    //1人目
     addContent: function() {
       if (this.newContent === "") return;
       var content = {
@@ -55,6 +101,7 @@ var app = new Vue({
       };
       this.content2.push(content);
       this.newContent = "";
+      this.saveContent2();
     },
     addMoney: function() {
       if (this.newMoney === "") return;
@@ -68,12 +115,28 @@ var app = new Vue({
         this.sum = parseInt(this.newMoney + this.sum);
       }
       this.newMoney = "";
+      this.saveMoney2();
     },
     deleteItem: function(index) {
-      this.sum = parseInt(this.sum - this.money2[index]);
+      // this.sum = parseInt(this.sum - this.money);
       this.money2.splice(index, 1);
       this.content2.splice(index, 1);
+      this.saveContent2();
+      this.saveMoney2();
     },
+    saveContent2() {
+      const parsed = JSON.stringify(this.content2);
+      localStorage.setItem("content2", parsed);
+    },
+    saveMoney2() {
+      const parsed = JSON.stringify(this.money2);
+      localStorage.setItem("money2", parsed);
+    },
+    saveSum() {
+      const parsed = JSON.stringify(this.sum);
+      localStorage.setItem("sum", parsed);
+    },
+    //2人目
     addContent2: function() {
       if (this.newContent2 === "") return;
       var content2 = {
@@ -81,6 +144,7 @@ var app = new Vue({
       };
       this.content3.push(content2);
       this.newContent2 = "";
+      this.saveContent3();
     },
     addMoney2: function() {
       if (this.newMoney2 === "") return;
@@ -94,6 +158,26 @@ var app = new Vue({
         this.sum2 = parseInt(this.newMoney2 + this.sum2);
       }
       this.newMoney2 = "";
+      this.saveMoney3();
+    },
+    deleteItem2: function(index) {
+      // this.sum = parseInt(this.sum - this.money);
+      this.money3.splice(index, 1);
+      this.content3.splice(index, 1);
+      this.saveContent3();
+      this.saveMoney3();
+    },
+    saveContent3() {
+      const parsed = JSON.stringify(this.content3);
+      localStorage.setItem("content3", parsed);
+    },
+    saveMoney3() {
+      const parsed = JSON.stringify(this.money3);
+      localStorage.setItem("money3", parsed);
+    },
+    saveSum2() {
+      const parsed = JSON.stringify(this.sum2);
+      localStorage.setItem("sum2", parsed);
     },
     totalMoney: function() {
       this.total = parseInt(this.sum + this.sum2);
